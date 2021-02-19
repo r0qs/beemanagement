f2f515567f74c778b3d5fe37fceea65434e5b4b3
Rodrigo Q. Saramago <rodrigoqsaramago@gmail.com>
2021-02-19 20:27:06 +0100

    Add initial scripts

---
+++ beemgmt.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+INSTALL_DIR="/usr/local/bin"
+SERVICE_DIR="/etc/systemd/system"
+
+start() {
+  echo "Enabling and starting timer"
+  systemctl enable bee_cashout.timer
+  systemctl start bee_cashout.timer
+}
+
+stop() {
+  echo "Disabling and stoping timer"
+  systemctl stop bee_cashout.timer
+  systemctl disable bee_cashout.timer
+}
+
+install() {
+  mkdir -p ${INSTALL_DIR}
+
+  echo "Installing Bee management scripts"
+  echo "Installing binary on ${INSTALL_DIR} ..."
+
+  cp dailycash.sh cashout.sh ${INSTALL_DIR}
+  chmod +x ${INSTALL_DIR}/dailycash.sh ${INSTALL_DIR}/cashout.sh
+
+  echo "Installing timer service on ${SERVICE_DIR}/bee_cashout.service ..."
+  cp ./systemd/bee_cashout.service ${SERVICE_DIR}
+
+  echo "Installing timer trigger on ${SERVICE_DIR}/bee_cashout.timer ..."
+  cp ./systemd/bee_cashout.timer ${SERVICE_DIR}
+
+  start
+}
+
+uninstall() {
+  stop
+
+  echo "Uninstalling Bee management scripts"
+  echo "Removing ${INSTALL_DIR}/bee_cashout ..."
+  rm -f ${INSTALL_DIR}/bee_cashout
+
+  echo "Removing ${SERVICE_DIR}/bee_cashout.service ..."
+  rm -f ${SERVICE_DIR}/bee_cashout.service
+
+  echo "Removing ${SERVICE_DIR}/bee_cashout.timer ..."
+  rm -f ${SERVICE_DIR}/bee_cashout.timer
+}
+
+usage()
+{
+  echo "Bee Management Scripts"
+  echo ""
+  echo "./beemgmt.sh"
+  echo "--help"
+  echo "--install"
+  echo "--remove"
+  echo "--start"
+  echo "--stop"
+  echo ""
+}
+
+while [ "$1" != "" ]; do
+  cmd=`echo $1 | awk -F= '{print $1}'`
+  case $cmd in
+    --help)
+      usage
+      exit
+      ;;
+    --install)
+      install
+      ;;
+    --remove)
+      uninstall
+      ;;
+    --start)
+      start
+      ;;
+    --stop)
+      stop
+      ;;
+    *)
+      echo "ERROR: unknown parameter \"$cmd\""
+      usage
+      exit 1
+      ;;
+  esac
+  shift
+done
---
+++ dailycash.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+CASHOUT_THRESHOLD=5
+source ./cashout.sh --crontab
+
+function givemoney() {
+  local n=0
+  local list=($(listAllUncashed))
+  for (( i=0; i<${#list[@]}; i+=2 ))
+  do
+    n=$(( n + 1 ))
+  done
+  if ((n >= ${CASHOUT_THRESHOLD})); then
+    cashoutAll ${MIN_AMOUNT}
+  fi
+}
+
+givemoney
